bca589a44d10d754dc544c82080619fedc2e8b83
jprd96 <jktpark1@gmail.com>
2022-12-04 15:58:32 -0800

    day 4

---
+++ 2022/solutions/python/4.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Advent of Code 2022 Day 4."""
+
+def read_file(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+def getChunks(line):
+    first, second = line.split(',')
+    x1, y1 = map(int, first.split('-'))
+    x2, y2 = map(int, second.split('-'))
+    return x1, y1, x2, y2
+
+def part1(filename):
+    lines = read_file(filename)
+    res = 0
+    for line in lines:
+        x1, y1, x2, y2 = getChunks(line)
+        if x1 <= x2 <= y2 <= y1:
+            res += 1
+        elif x2 <= x1 <= y1 <= y2:
+            res += 1
+    return res
+
+def part2(filename):
+    lines = read_file(filename)
+    res = len(lines)
+    for line in lines:
+        x1, y1, x2, y2 = getChunks(line)
+        if x1 > y2 or x2 > y1:
+            res -= 1
+    return res
+
+def main():
+    inputFile = "../../inputs/4.txt"
+    print(part1(inputFile))
+    print(part2(inputFile))
+
+if __name__ == '__main__':
+    main()
---
+++ 2022/solutions/swift/lolmaybe.swift
@@ -0,0 +1,10 @@
+//
+//  lolmaybe.swift
+//  
+//
+//  Created by James K. Park on 12/3/22.
+//
+
+import Foundation
+
+print("test!")
